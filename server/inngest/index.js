120b1a0f06a538a9c331b48c7966b54a5b669dfd
arshitgajera24 <arshitgajera24@gmail.com>
2025-10-20 10:53:41 +0530

    First Commit

---
+++ server/inngest/index.js
@@ -0,0 +1,64 @@
+import { Inngest } from "inngest";
+import User from "../models/User.js";
+
+export const inngest = new Inngest({ id: "sharp-link" });
+
+//* Save User data to Database
+const syncUserCreation = inngest.createFunction(
+    { id: 'sync-user-from-clerk' },
+    { event: "clerk/user.created" },
+    async ({event}) => {
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
+        let username = email_addresses[0].email_address.split("@")[0];
+
+        // Check Availability of Username
+        const user = await User.findOne({username});
+        if(user) {
+            username = username + Math.floor(Math.random() * 10000);
+        }
+
+        const userData = {
+            _id: id,
+            email: email_addresses[0].email_address,
+            full_name: first_name + " " + last_name,
+            profile_picture: image_url,
+            username
+        }
+        await User.create(userData);
+    }
+)
+
+//* Update User data to Database
+const syncUserUpdation = inngest.createFunction(
+    { id: 'update-user-from-clerk' },
+    { event: "clerk/user.updated" },
+    async ({event}) => {
+        const {id, first_name, last_name, email_addresses, image_url} = event.data;
+
+        const updateUserData = {
+            email: email_addresses[0].email_address,
+            full_name: first_name + " " + last_name,
+            profile_picture: image_url,
+        }
+        await User.findByIdAndUpdate(id, updateUserData);
+    }
+)
+
+
+//* Delete User data to Database
+const syncUserDeletion = inngest.createFunction(
+    { id: 'delete-user-with-clerk' },
+    { event: "clerk/user.deleted" },
+    async ({event}) => {
+        const {id} = event.data;
+
+        await User.findByIdAndDelete(id);
+    }
+)
+
+
+export const functions = [
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+];
---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import connectDB from "./config/db.js";
+import {inngest, functions} from "./inngest/index.js"
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+await connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => res.send("Backend Running Yeeeee !!"))
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
+
+app.listen(PORT, () => {
+    console.log(`🔥 Server is Running on port http://localhost:${PORT}`);
+})
+
