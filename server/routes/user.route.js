f4c10713ed62d1e3a5d7b42805d8f657fe794349
arshitgajera24 <arshitgajera24@gmail.com>
2025-10-20 12:49:33 +0530

    next commit

---
+++ server/routes/user.route.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import * as userControllers from "../controllers/user.controller.js";
+import { protect } from "../middleware/auth.js";
+import { upload } from "../config/multer.js";
+
+const userRouter = Router();
+
+userRouter.route("/data").get(protect, userControllers.getUserData);
+userRouter.route("/update").post(upload.fields([{name: "profile", maxCount: 1}, {name: "cover", maxCount: 1}]), protect, userControllers.updateUserData);
+userRouter.route("/discover").post(protect, userControllers.discoverUsers);
+userRouter.route("/follow").post(protect, userControllers.followUser);
+userRouter.route("/unfollow").post(protect, userControllers.unfollowUser);
+
+export default userRouter;
