38107df834bb9cea91684d55d03721904fd8519e
arshitgajera24 <arshitgajera24@gmail.com>
2025-10-26 21:10:29 +0530

    video post

--- client/src/components/Storiesbar.jsx
+++ client/src/components/Storiesbar.jsx
@@ -18,9 +18,11 @@ const Storiesbar = () => {
     const [myStory, setMyStory] = useState(null);
     const [otherStories, setOtherStories] = useState([]);
     const [loaded, setLoaded] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     const fetchStories = async () => {
         try {
+            setLoading(true);
             const token = await getToken();
             const {data} = await api.get("/api/story/get", {
                 headers: {
@@ -56,8 +58,17 @@ const Storiesbar = () => {
             }
         } catch (error) {
             toast.error(error.message)
+        } finally {
+            setLoading(false);
         }
     }
+
+    const StorySkeleton = () => (
+        <div className="flex flex-col items-center animate-pulse">
+            <div className="relative size-16 sm:size-18 md:size-20 rounded-full bg-gradient-to-tr from-indigo-400 to-purple-400 opacity-50" />
+            <div className="h-3 w-12 bg-gray-300 rounded mt-2" />
+        </div>
+    );
 
     useEffect(() => {
         fetchStories();
@@ -79,77 +90,88 @@ const Storiesbar = () => {
     <div className="w-full sm:w-[calc(100vw-240px)] lg:max-w-2xl mx-auto overflow-x-auto no-scrollbar px-4 py-2">
         <div className="flex items-center gap-4">
 
-        {/* My Story or Add Story */}
-        {
-            !myStory ? (
-                <div onClick={() => setShowModel(true)} className="flex flex-col items-center cursor-pointer group">
-                    <div className="relative size-16 sm:size-18 md:size-20 rounded-full bg-gradient-to-tr from-indigo-500 to-purple-600 p-[2px]">
-                        <div className="bg-white rounded-full size-full flex items-center justify-center">
-                            <div className="size-12 sm:size-14 md:size-16 bg-indigo-500 rounded-full flex items-center justify-center transition-transform group-active:scale-90">
-                                <Plus className="w-6 h-6 text-white" />
+        {loading ? (
+            // Show shimmer while stories are loading
+            <>
+                {[...Array(4)].map((_, i) => (
+                    <StorySkeleton key={i} />
+                ))}
+            </>
+        ) : (
+            <>
+                {/* My Story or Add Story */}
+                {
+                    !myStory ? (
+                        <div onClick={() => setShowModel(true)} className="flex flex-col items-center cursor-pointer group">
+                            <div className="relative size-16 sm:size-18 md:size-20 rounded-full bg-gradient-to-tr from-indigo-500 to-purple-600 p-[2px]">
+                                <div className="bg-white rounded-full size-full flex items-center justify-center">
+                                    <div className="size-12 sm:size-14 md:size-16 bg-indigo-500 rounded-full flex items-center justify-center transition-transform group-active:scale-90">
+                                        <Plus className="w-6 h-6 text-white" />
+                                    </div>
+                                </div>
                             </div>
+                            <p className="text-xs sm:text-sm text-gray-600 mt-1 font-medium">Add Story</p>
                         </div>
-                    </div>
-                    <p className="text-xs sm:text-sm text-gray-600 mt-1 font-medium">Add Story</p>
-                </div>
-            ) : (
-                <div className="flex flex-col items-center cursor-pointer group relative"
-                    onClick={() => {
-                        setSelectedStoryId(myStory.stories[0]._id);
-                        setViewStory(myStory.stories);
-                    }} >
-                    <div className="relative size-16 sm:size-18 md:size-20 rounded-full p-[2px] bg-gradient-to-tr from-indigo-500 to-purple-600">
-                        <div className="bg-white rounded-full size-full flex items-center justify-center relative">
-                            <img src={myStory.user.profile_picture} alt={myStory.user.full_name} className="size-14 sm:size-16 md:size-18 rounded-full object-cover border-2 border-white transition-transform duration-300 group-hover:scale-105" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
-                            <button onClick={(e) => {
-                                e.stopPropagation();
-                                setShowModel(true);
-                            }} className="absolute bottom-0 right-0 bg-indigo-600 text-white rounded-full p-1 shadow-md hover:bg-indigo-700 transition cursor-pointer">
-                                <Plus className="w-3 h-3" />
-                            </button>
-                        </div>
-                    </div>
-                    <p className="text-xs sm:text-sm text-gray-700 mt-1 font-medium">Your Story</p>
-                </div>
-            )
-        }
-
-        {/* Other User's Stories */}
-        {
-            otherStories.map((story, index) => {
-                const totalStories = story.stories.length;
-                const angle = 360 / totalStories;
-                const gap = totalStories === 1 ? 0 : 2;
-
-                const gradientSegments = story.stories.map((_, i) => {
-                    const start = i * angle + gap;
-                    const end = (i + 1) * angle - gap;
-                    const colors = ['#5e35b1', '#9c27b0', '#7e57c2', '#673ab7', '#8e24aa', '#d500f9'];
-                    const color = colors[i % colors.length];
-                    return `${color} ${start}deg ${end}deg, transparent ${end}deg ${(i + 1) * angle}deg`;
-                }).join(', ');
-
-                return (
-                    <div key={index} onClick={() => {
-                        setSelectedStoryId(story.stories[0]._id);
-                        setViewStory(story.stories);
-                    }} className="flex flex-col items-center cursor-pointer group">
-                        <div className="relative size-16 sm:size-18 md:size-20 rounded-full p-[2px]"
-                            style={{
-                                background: `conic-gradient(${gradientSegments})`,
-                                boxShadow: '0 0 6px rgba(0,0,0,0.3)',
+                    ) : (
+                        <div className="flex flex-col items-center cursor-pointer group relative"
+                            onClick={() => {
+                                setSelectedStoryId(myStory.stories[0]._id);
+                                setViewStory(myStory.stories);
                             }} >
-                            <div className="bg-white rounded-full size-full flex items-center justify-center">
-                            <img src={story.user.profile_picture} alt={story.user.full_name} className="size-14 sm:size-16 md:size-18 rounded-full object-cover border-2 border-white transition-transform duration-300 group-hover:scale-105" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
+                            <div className="relative size-16 sm:size-18 md:size-20 rounded-full p-[2px] bg-gradient-to-tr from-indigo-500 to-purple-600">
+                                <div className="bg-white rounded-full size-full flex items-center justify-center relative">
+                                    <img src={myStory.user.profile_picture} alt={myStory.user.full_name} className="size-14 sm:size-16 md:size-18 rounded-full object-cover border-2 border-white transition-transform duration-300 group-hover:scale-105" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
+                                    <button onClick={(e) => {
+                                        e.stopPropagation();
+                                        setShowModel(true);
+                                    }} className="absolute bottom-0 right-0 bg-indigo-600 text-white rounded-full p-1 shadow-md hover:bg-indigo-700 transition cursor-pointer">
+                                        <Plus className="w-3 h-3" />
+                                    </button>
+                                </div>
                             </div>
+                            <p className="text-xs sm:text-sm text-gray-700 mt-1 font-medium">Your Story</p>
                         </div>
-                        <p className="text-xs sm:text-sm text-gray-700 mt-1 truncate max-w-[70px] text-center">
-                            {story.user.full_name.split(" ")[0]}
-                        </p>
-                    </div>
-                );
-            })
-        }
+                    )
+                }
+
+                {/* Other User's Stories */}
+                {
+                    otherStories.map((story, index) => {
+                        const totalStories = story.stories.length;
+                        const angle = 360 / totalStories;
+                        const gap = totalStories === 1 ? 0 : 2;
+
+                        const gradientSegments = story.stories.map((_, i) => {
+                            const start = i * angle + gap;
+                            const end = (i + 1) * angle - gap;
+                            const colors = ['#5e35b1', '#9c27b0', '#7e57c2', '#673ab7', '#8e24aa', '#d500f9'];
+                            const color = colors[i % colors.length];
+                            return `${color} ${start}deg ${end}deg, transparent ${end}deg ${(i + 1) * angle}deg`;
+                        }).join(', ');
+
+                        return (
+                            <div key={index} onClick={() => {
+                                setSelectedStoryId(story.stories[0]._id);
+                                setViewStory(story.stories);
+                            }} className="flex flex-col items-center cursor-pointer group">
+                                <div className="relative size-16 sm:size-18 md:size-20 rounded-full p-[2px]"
+                                    style={{
+                                        background: `conic-gradient(${gradientSegments})`,
+                                        boxShadow: '0 0 6px rgba(0,0,0,0.3)',
+                                    }} >
+                                    <div className="bg-white rounded-full size-full flex items-center justify-center">
+                                    <img src={story.user.profile_picture} alt={story.user.full_name} className="size-14 sm:size-16 md:size-18 rounded-full object-cover border-2 border-white transition-transform duration-300 group-hover:scale-105" loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
+                                    </div>
+                                </div>
+                                <p className="text-xs sm:text-sm text-gray-700 mt-1 truncate max-w-[70px] text-center">
+                                    {story.user.full_name.split(" ")[0]}
+                                </p>
+                            </div>
+                        );
+                    })
+                }
+            </>
+        )}
         </div>
 
         {/* Modals */}
@@ -163,6 +185,8 @@ const Storiesbar = () => {
                 />
             )
         }
+
+        
     </div>
   );
 };
