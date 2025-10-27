0cfaacfd33882b5bcad7d3452cba039289d52e0d
arshitgajera24 <arshitgajera24@gmail.com>
2025-10-27 20:35:24 +0530

    chat media open

--- client/src/components/OpenMediaChat.jsx
+++ client/src/components/OpenMediaChat.jsx
@@ -1,10 +1,20 @@
 import { ArrowLeft } from 'lucide-react'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import Loading from './Loading';
 
 const OpenMediaChat = ({setShowMedia, showMedia}) => {
 
+    const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        document.body.style.overflow = "hidden";
+        return () => {
+            document.body.style.overflow = "auto";
+        };
+    }, []);
+
   return (
-    <div className='fixed inset-0 z-110 bg-black/70 backdrop-blur-sm text-white flex items-center justify-center p-4'>
+    <div className='fixed inset-0 z-[9999] bg-black/90 text-white flex flex-col items-center justify-center p-4 transition-opacity duration-300'>
         <div className='w-full max-w-md flex flex-col'>
             <div className='flex items-center justify-between mb-4'>
                 <button onClick={() => setShowMedia(null)} className='text-white p-2 cursor-pointer'>
@@ -13,9 +23,12 @@ const OpenMediaChat = ({setShowMedia, showMedia}) => {
                 <span className='w-10'></span>
             </div>
 
-            <div className='rounded-lg h-[80vh] flex items-center justify-center bg-black'>
+            <div className='relative w-full h-[80vh] flex items-center justify-center bg-black rounded-lg overflow-hidden'>
                 {
-                    showMedia && <img src={showMedia} alt="Chat Media" className='object-contain max-h-full max-w-full transition-all' loading='lazy' decoding="async" />
+                    !loaded && <Loading />
+                }
+                {
+                    showMedia && <img src={showMedia} alt="Chat Media" className={`object-contain max-h-full max-w-full transition-opacity duration-500 ${loaded ? 'opacity-100' : 'opacity-0'}`} decoding="async" onLoad={() => setLoaded(true)} />
                 }
             </div>
         </div>
