2026e9fc1c6e4169f8251b4dcfdbadbe698d30f3
arshitgajera24 <arshitgajera24@gmail.com>
2025-10-27 18:14:13 +0530

    delete msg through sse

--- client/src/pages/Chatbox.jsx
+++ client/src/pages/Chatbox.jsx
@@ -175,7 +175,7 @@ const Chatbox = ({ selectedUserId, onBack }) => {
   };
 
   return user && (
-    <div className='flex flex-col flex-1 max-h-[90vh] overflow-x-hidden'>
+    <div className='flex flex-col flex-1 max-h-[94.5vh] overflow-x-hidden'>
       <div onClick={() => { navigate(`/profile/${userId}`); dispatch(closeChat()); }} className='flex items-center gap-2 p-2 md:px-10 bg-white border-b border-gray-300 sticky top-0 z-10 cursor-pointer'>
         <button onClick={(e) => {e.stopPropagation(); onBack && onBack(); }}>
           <ArrowLeft className='w-6 h-6 hover:scale-110 active:scale-95 mr-2 cursor-pointer' />
