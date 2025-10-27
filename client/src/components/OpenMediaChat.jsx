e4a9644bc2eabf54cf4154efc13619ddb4d8bec5
arshitgajera24 <arshitgajera24@gmail.com>
2025-10-27 20:25:26 +0530

    stories

--- client/src/components/OpenMediaChat.jsx
+++ client/src/components/OpenMediaChat.jsx
@@ -6,18 +6,18 @@ const OpenMediaChat = ({setShowMedia, showMedia}) => {
     const [loaded, setLoaded] = useState(false);
 
   return (
-    <div className='fixed inset-0 z-110 min-h-screen bg-black/50 backdrop-blur text-white flex items-center justify-center p-4 -mt-14 md:-mt-0'>
-        <div className='w-full max-w-md'>
-            <div className='text-center mb-4 flex items-center justify-between'>
+    <div className='fixed inset-0 z-110 bg-black/70 backdrop-blur-sm text-white flex items-center justify-center p-4'>
+        <div className='w-full max-w-md flex flex-col'>
+            <div className='flex items-center justify-between mb-4'>
                 <button onClick={() => setShowMedia(null)} className='text-white p-2 cursor-pointer'>
                     <ArrowLeft />
                 </button>
                 <span className='w-10'></span>
             </div>
 
-            <div className='rounded-lg h-140 sm:h-140 md:h-160 flex items-center justify-center relative bg-black'>
+            <div className='rounded-lg h-[80vh] flex items-center justify-center bg-black'>
                 {
-                    showMedia && <img src={showMedia} alt="Chat Media" className='object-contain max-h-full' loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
+                    showMedia && <img src={showMedia} alt="Chat Media" className='object-contain max-h-full max-w-full transition-all' loading='lazy' decoding="async" onLoad={() => setLoaded(true)} style={{filter: loaded ? "none" : "blur(20px)", transition: "filter 0.3s ease-out"}} />
                 }
             </div>
         </div>
