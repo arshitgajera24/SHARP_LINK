dac4057d99d9dd61423258701ac51eb05b8041bb
arshitgajera24 <arshitgajera24@gmail.com>
2025-11-28 10:05:55 +0530

    redis added

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,17 +1,25 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-    host: "127.0.0.1",
-    post: 6379,
-});
+let redis;
+
+if (process.env.REDIS_URL) {
+    redis = new Redis(process.env.REDIS_URL, {
+        tls: {},
+    });
+} else {
+    redis = new Redis({
+        host: process.env.REDIS_HOST || "127.0.0.1",
+        port: process.env.REDIS_PORT || 6379,
+        password: process.env.REDIS_PASSWORD || undefined
+    });
+}
 
 redis.on("connect", () => {
-    console.log("Redis Connected");
-})
+    console.log("🔗 Redis connected");
+});
 
 redis.on("error", (err) => {
-    console.error("Redis Error : ", err);
-})
+    console.log("❌ Redis error:", err.message);
+});
 
 export default redis;
-
